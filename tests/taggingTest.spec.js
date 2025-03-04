c1287f9a4dbafac3c0c246ee2439567383b2db50
Junaid Khan <junnis111@gmail.com>
2025-03-05 02:59:14 +0500

    tagging-test

--- tests/taggingTest.spec.js
+++ tests/taggingTest.spec.js
@@ -1,32 +1,24 @@
 const {test , expect} = require('@playwright/test')
 
 test('test-1@fast', async ({page})=>{
-    console.log("This is my Test-1");
-   await page.waitForTimeout(1000) 
+    console.log("This is my Test-1")
 })
 test('test-2@fast', async ({page})=>{
-    console.log("This is my Test-2");
-    await page.waitForTimeout(1000) 
+    console.log("This is my Test-2")
 })
 
 test('test-3@slow' , async ({page})=>{
     console.log("This is my Test-3");
-    
-    await page.waitForTimeout(1000) 
 })
 
 test('test-4@slow' , async ({page})=>{
-    console.log("This is my Test-4");
-    
-    await page.waitForTimeout(1000) 
+    console.log("This is my Test-4"); 
 })
 
 test('test-5@fast@slow' , async ({page})=>{
     console.log("This is my Test-5");
-    
-    await page.waitForTimeout(1000) 
 })
 
-// npx playwright test tests/taggingTest.spec.js --project chromium --grep "@fast"
+// npx playwright test tests/taggingTest.spec.js --project chromium --grep "@
 // npx playwright test tests/taggingTest.spec.js --project chromium --grep "@fast|@slow"
 // npx playwright test tests/taggingTest.spec.js --project chromium --grep "@fast" --grep-invert "@slow"

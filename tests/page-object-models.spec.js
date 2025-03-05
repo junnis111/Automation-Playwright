b3821bdd76bc1f0620cc84839c53b0e2eb8243c2
Junaid Khan <junnis111@gmail.com>
2025-03-05 09:10:26 +0500

    pageobject-models

---
+++ tests/page-object-models.spec.js
@@ -0,0 +1,17 @@
+const {test , expect} = require('@playwright/test')
+import LoginPage from '../pages/login'
+import HomePage from '../pages/homepage'
+
+
+
+test('page-object-models', async({page})=>{
+    
+    // login-page
+    const login = new LoginPage(page)
+    await login.gotoLoginPage()
+    await login.login('pavanol', 'test@123')
+
+    // home-page
+    const homepage = new HomePage(page)
+    await homepage.addProductToCart("Samsung galaxy s6")
+})
